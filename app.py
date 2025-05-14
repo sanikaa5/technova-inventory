d5ed66af594744353fd4e9a2070419bc09cc4a86
Sanika Dhakite <167352722+sanikaa5@users.noreply.github.com>
2025-05-14 23:34:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Technova Inventory'
+def inventory():
+    return 'Technova Inventory: '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
