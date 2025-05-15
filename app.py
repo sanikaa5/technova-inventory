d4abe83ceda51a7cf224d4d5aa189f1f2613642e
Sanika Dhakite <167352722+sanikaa5@users.noreply.github.com>
2025-05-15 09:25:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Inventory App Running"
+    return "Inventory App Running..."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
